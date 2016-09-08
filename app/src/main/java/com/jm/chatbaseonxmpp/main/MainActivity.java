dfd4379897522ca783bd2aa1f223d6e847ed1491
会说话的木头 <441509188@qq.com>
2016-09-08 15:17:29 +0800

    分包

--- app/src/main/java/com/jm/chatbaseonxmpp/main/MainActivity.java
+++ app/src/main/java/com/jm/chatbaseonxmpp/main/MainActivity.java
@@ -1,4 +1,4 @@
-package com.jm.chatbaseonxmpp;
+package com.jm.chatbaseonxmpp.main;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.jm.chatbaseonxmpp.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
