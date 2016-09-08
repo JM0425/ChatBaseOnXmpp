0780e7f0451d5b512a383769939f45cde2542a9f
会说话的木头 <441509188@qq.com>
2016-09-08 16:47:01 +0800

    增加基类

---
+++ app/src/main/java/com/jm/chatbaseonxmpp/base/BaseAct.java
@@ -0,0 +1,11 @@
+package com.jm.chatbaseonxmpp.base;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * User: litengfei
+ * Date: 2016-09-08
+ * Time: 16:45
+ */
+public class BaseAct extends FragmentActivity{
+} 
---
+++ app/src/main/java/com/jm/chatbaseonxmpp/base/BaseApplication.java
@@ -0,0 +1,11 @@
+package com.jm.chatbaseonxmpp.base;
+
+import android.app.Application;
+
+/**
+ * User: litengfei
+ * Date: 2016-09-08
+ * Time: 16:45
+ */
+public class BaseApplication extends Application {
+} 
